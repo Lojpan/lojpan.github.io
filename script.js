25945d1451dd6191dd18f3153d9389846247c0c4
Lojpan <loi@reflectionmusic.se>
2020-08-30 00:37:20 +0200

    Pushing tag nr to web

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 var element = document.getElementById("checkInBox");
-var displayTag;
-var tag_temp = document.createTextNode(displayTag);
+//var displayTag;
+var tempTag = 0;
+var tag_temp = document.createTextNode(tempTag.toString);
 element.appendChild(tag_temp);
