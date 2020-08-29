47f8fb168dc1a31fcce4a13d9c5f23e3b6ac94ab
Lojpan <loi@reflectionmusic.se>
2020-08-30 01:30:22 +0200

    code changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-//var element = document.getElementById("checkInBox");
+var element = document.getElementById("checkInBox");
 tempTag = 10;
 //var tempTag = tempTag.toString();
-let tag_div = document.createElement('checkInBox');
-tag_div.innerHTML = `${tempTag}`;   
-tagTemp_div.appendChild(tag_div);
+//let tag_div = document.createElement('checkInBox');
+element.innerHTML = `${tempTag}`;   
+tagTemp_div.appendChild(element);
