bb672f1b07b3549cacd3a36c60b8c4b055831b82
Lojpan <loi@reflectionmusic.se>
2020-08-30 06:11:54 +0200

    svds

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function displayTag(){
 var tempTag = 10;
 var element = document.getElementById("checkInBox");
 //element.appendChild(newTag); 
-element.innerHTML(newTag);
+element.innerHTML = `${newTag}`
 }
