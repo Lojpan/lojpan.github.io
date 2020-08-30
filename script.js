730690b2312586c932779aa4e2590168a322ee71
Lojpan <loi@reflectionmusic.se>
2020-08-30 04:32:42 +0200

    davadva

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 function displayTag(){
-var element = document.getElementById("checkInBox");
-//var tempTag = tempTag.toString();
-//let tag_div = document.createElement('checkInBox');
 var tempTag = 10;
+var element = document.getElementById("checkInBox");
 element.innerHTML = `${tempTag}`;   
-//element.appendChild(element);
 }
