815e27a0906b2be2578c1b7828261be343d7acf7
Lojpan <loi@reflectionmusic.se>
2020-09-04 21:02:28 +0200

    new reload page function

--- myScript.js
+++ myScript.js
@@ -1,7 +1,27 @@
+
+
 function displayTag(){
 let element = document.getElementById("checkInBox").innerHTML = `${name}`;
+    reload();
 }
+
+function reload(){
+    if (newCheckin)
+    window.location.reload();
+}
+
+
 /*
+<script> 
+$(document).ready(function(){
+setInterval(function(){
+      $("checkInBox").load("checkInBox" );
+}, 3000);
+});
+</script>
+
+<div id="here">dynamic content ?</div>
+
 setTimeout(function (){
     window.location.reload();
  },10000);
