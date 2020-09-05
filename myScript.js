b784411a6b5bf9950d621268cd9cbd21b00035cb
Lojpan <loi@reflectionmusic.se>
2020-09-05 18:09:39 +0200

    saca

--- myScript.js
+++ myScript.js
@@ -1,26 +1,34 @@
 
 
-
-function refreshDiv() {
-    //$('#checkInBox').load('#checkInBox');
-    window.location.reload();
+function updateDiv(){
+    var name_temp = document.getElementById("checkInBox").innerHTML = `${name}`; 
+    while(name_temp == ""){
+        setInterval(
+            function () {
+                $('#checkInBox').load('#checkInBox');
+            }, 3000);
+    }
+    displayTag();
 }
 
 function displayTag(){
     document.getElementById("checkInBox").innerHTML = `${name}`;
+            //refreshDiv();
 }
 
+
+function refreshDiv() {
+    //document.getElementById("checkInBox").innerHTML = `${name}`;
+    $('#checkInBox').load('#checkInBox');
+    //window.location.reload();
+}
 
 
 
 /*
 
 function displayTag(){
-    var newCheckIn = document.getElementById("checkInBox") = `${newCheckIn}`;
     document.getElementById("checkInBox").innerHTML = `${name}`;
-        if(newCheckIn = 1){
-            refreshDiv();
-        }
 }
 
 function refreshDiv(){
@@ -29,6 +37,12 @@ function refreshDiv(){
             $('#checkInBox').load('#checkInBox');
         }, 500);
 }
+
+var newCheckIn = document.getElementById("checkInBox") = `${newCheckIn}`;
+
+
+
+
 */
 
 
