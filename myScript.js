6f08a8f363ddf2f264d442984cec34fc99eb914d
Lojpan <loi@reflectionmusic.se>
2020-09-05 10:44:53 +0200

    working script with auto refresh function

--- myScript.js
+++ myScript.js
@@ -12,31 +12,3 @@ function refreshDiv(){
 
 
 
-
-
-
-
-
-
-/*
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
-<script type="text/javascript">
-var auto_refresh = setInterval(
-function () {
-    $('#div').load('#div');
-}, 1000);
-</script>
-
-
-function reloadDiv(){
-   document.getElementById("checkInBox").innerHTML = `${newCheckin}`;
-    //newCheckin = document.getElementById("id").value
-    //if (`${newCheckin}` ==  1 ){
-    //window.location.reload();
-    //newCheckin = 0;
-    //}
-}
-*/
-
-
-
