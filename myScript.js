b4a8376355bc62b33f2084e894628031c6254d9e
Lojpan <loi@reflectionmusic.se>
2020-09-05 10:38:55 +0200

    xv

--- myScript.js
+++ myScript.js
@@ -3,6 +3,31 @@ function displayTag(){
     document.getElementById("checkInBox").innerHTML = `${name}`;
 }
 
+function refreshDiv(){
+    setInterval(
+        function () {
+            $('#checkInBox').load('#checkInBox');
+        }, 500);
+}
+
+
+
+
+
+
+
+
+
+/*
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
+<script type="text/javascript">
+var auto_refresh = setInterval(
+function () {
+    $('#div').load('#div');
+}, 1000);
+</script>
+
+
 function reloadDiv(){
    document.getElementById("checkInBox").innerHTML = `${newCheckin}`;
     //newCheckin = document.getElementById("id").value
@@ -11,7 +36,7 @@ function reloadDiv(){
     //newCheckin = 0;
     //}
 }
-
+*/
 
 
 
