774bc1f2773406015016b0f126283f627481886d
Lojpan <loi@reflectionmusic.se>
2020-09-05 14:31:27 +0200

    new refresh div script

--- myScript.js
+++ myScript.js
@@ -3,12 +3,18 @@ function displayTag(){
     document.getElementById("checkInBox").innerHTML = `${name}`;
 }
 
+function refreshDiv() {
+    $('#checkInBox').load('#checkInBox');
+}
+
+
+/*
 function refreshDiv(){
     setInterval(
         function () {
             $('#checkInBox').load('#checkInBox');
         }, 500);
 }
-
+*/
 
 
